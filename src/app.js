500866b5ca46c96b4ebdfbdf939c1edbbe956c13
Vladislav <vladikyssjil@gmail.com>
2018-11-02 12:18:31 +0200

    update

--- src/app.js
+++ src/app.js
@@ -2,14 +2,14 @@ document.addEventListener('DOMContentLoaded', Tab);
 
 
 function Tab() {
-  let lengthTabs = document.querySelectorAll('.tabs');
-  let lengthContent = document.querySelectorAll('.tab-content');
-  for (let i = 0; i< lengthTabs.length; i++) {
-    lengthTabs[i].addEventListener('click', function() {
-      for (let j = 0; j < lengthTabs.length; j++) {
-        lengthContent[j].classList.remove('active');
+  let tabs = document.querySelectorAll('.tabs');
+  let content = document.querySelectorAll('.tab-content');
+  for (let i = 0; i< tabs.length; i++) {
+    tabs[i].addEventListener('click', function() {
+      for (let j = 0; j < tabs.length; j++) {
+        content[j].classList.remove('active');
       }
-      lengthContent[i].classList.add('active');
+      content[i].classList.add('active');
     });
   }
 }
